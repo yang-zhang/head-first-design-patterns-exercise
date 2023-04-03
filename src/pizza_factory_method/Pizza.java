377d8b9776e2b91ceced9552e37cd64cdd50b2e0
Yang Zhang <yang.zhang@faire.com>
2023-04-03 07:51:09 -0400

    Add pizza factory method

---
+++ src/pizza_factory_method/Pizza.java
@@ -0,0 +1,37 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public abstract class Pizza {
+	String name;
+	String dough;
+	String sauce;
+
+	List<String> toppings = new ArrayList<String>();
+
+	void prepare() {
+		System.out.println("Prepare " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings: ");
+		for (String topping : toppings) {
+			System.out.println("   " + topping);
+		}
+	}
+  
+	void bake() {
+		System.out.println("Bake for 25 minutes at 350");
+	}
+ 
+	void cut() {
+		System.out.println("Cut the pizza into diagonal slices");
+	}
+  
+	void box() {
+		System.out.println("Place pizza in official PizzaStore box");
+	}
+ 
+	public String getName() {
+		return name;
+	}
+}
+	
