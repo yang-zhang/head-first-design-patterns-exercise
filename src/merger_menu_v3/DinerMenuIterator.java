bba4227a95c6ba86721c33a29ed54848681202d3
Yang Zhang <yang.zhang@faire.com>
2023-04-23 12:07:14 -0400

    Add merger menu v3

---
+++ src/merger_menu_v3/DinerMenuIterator.java
@@ -0,0 +1,29 @@
+import java.util.Iterator;
+
+public class DinerMenuIterator implements Iterator<MenuItem> {
+    MenuItem[] items;
+    int position = 0;
+
+    public DinerMenuIterator(MenuItem[] items) {
+        this.items = items;
+    }
+
+    public MenuItem next() {
+        MenuItem menuItem = items[position];
+        position = position + 1;
+        return menuItem;
+    }
+
+    public boolean hasNext() {
+        if (position >= items.length || items[position] == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void remove() {
+        throw new UnsupportedOperationException
+                ("You shouldn't be trying to remove menu items.");
+    }
+}
---
+++ src/merger_menu_v3/DinerMenuTestDrive.java
@@ -0,0 +1,12 @@
+public class DinerMenuTestDrive {
+    public static void main(String[] args) {
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
+        CafeMenu cafeMenu = new CafeMenu();
+
+        Waitress waitress = new Waitress(pancakeHouseMenu, dinerMenu, cafeMenu);
+
+        waitress.printMenu();
+    }
+
+}
